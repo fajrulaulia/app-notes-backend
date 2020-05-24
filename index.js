df69053ce95bdb7223773e709e883ff0183339d3
fajrulaulia <auliafajrul7@outlook.co.id>
2020-05-25 00:49:31 +0700

    add cors

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
-
+const cors = require('cors')
 const route = require('./src/route')
 require('./src/configs/database').connect()
 require('dotenv').config()
 
 app.use(bodyParser.json())
+app.use(cors)
 app.use(route)
 app.listen(process.env.EXPRESS_PORT, () =>
     console.log(`App running on port ${process.env.EXPRESS_PORT}`))
