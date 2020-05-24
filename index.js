6e0408a8b564cc048e9194a8dd1316348f0712a1
fajrulaulia <auliafajrul7@outlook.co.id>
2020-05-25 01:34:12 +0700

    change to alpine edge and update npm & nodejs to 12xx

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('./src/configs/database').connect()
 require('dotenv').config()
 
 app.use(bodyParser.json())
-app.use(cors)
+app.use(cors())
 app.use(route)
 app.listen(process.env.EXPRESS_PORT, () =>
     console.log(`App running on port ${process.env.EXPRESS_PORT}`))
